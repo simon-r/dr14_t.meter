f04b9ffdab7590433ef9815cfe030711d02408a7
simon-r <simone.rva@gmail.com>
2012-01-12 22:28:43 +0100

    ajax test

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,48 @@
-function dr14_version() {
-	document.write( "Ver.: 0.7.3" ) 
-}
+
+
+
 
 function mail_to_me() {
 
-	var mm = "dr14"
-	mm += ".tmeter"
-	mm += "@"
-	mm += "gmail.com" 		
+	var mm = "dr14" ;
+	mm += ".tmeter" ;
+	mm += "@" ;
+	mm += "gmail.com" ; 		
 	
 	var monmail="<a href=mailto:";
 	monmail += mm ;
 	monmail += "?subject=Feedback>";
-	document.write(monmail+ mm + "</a>");
+	document.write(monmail + mm + "</a>");
 
+}
+
+
+function dr14_version() {
+	document.write( "Ver.: 0.7.3" ) ;
+}
+
+var http = createRequestObject();
+
+function createRequestObject() {
+   var objAjax;
+   var browser = navigator.appName;
+   if(browser == "Microsoft Internet Explorer"){
+      objAjax = new ActiveXObject("Microsoft.XMLHTTP");
+   }else{
+      objAjax = new XMLHttpRequest();
+   }
+   return objAjax;
+}
+
+function getNewContent( page_name ){
+   http.open( 'get' , page_name );
+   http.onreadystatechange = updateNewContent;
+   http.send(null);
+   return false;
+}
+
+function updateNewContent(){
+   if(http.readyState == 4){
+      document.getElementById('mySentence').innerHTML = http.responseText;
+   }
 }
