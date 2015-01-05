f4c98a0df190a8ecfb573b7ee636a80522a15c3b
Simone Riva <simone.rva@gmail.com>
2015-01-05 15:25:36 +0100

    fix

--- dr14tmeter/read_metadata.py
+++ dr14tmeter/read_metadata.py
@@ -245,8 +245,7 @@ class RetirveMetadata:
         
     def scan_file_avprobe( self , file_name ):
         try:
-            data_txt = subprocess.check_output( [ self.__ffprobe_cmd , " -show_format " , file_name ] , 
-                                                stderr=subprocess.STDOUT , shell=False )
+            data_txt = subprocess.check_output( [ self.__ffprobe_cmd , "-show_format" , file_name ] , stderr=subprocess.STDOUT , shell=False )
         except :
             data_txt = "ffprobe ERROR"
          
